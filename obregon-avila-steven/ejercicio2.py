68576b4cc56481ac36c8d2c8da0ca0ae0f769bc6
code1077 <primaveraazul36@gmail.com>
2018-04-04 14:02:51 +0200

    Mi ejercicio 2

---
+++ obregon-avila-steven/ejercicio2.py
@@ -0,0 +1,12 @@
+#Lo usamos para comentarios..
+edad = 0
+nombre= ""
+#Para preguntar la edad..
+edad = int(input("Que edad tienes:\n"))
+#Comprobamos si el usuario es mayor o menor de eadd
+if edad > 17:
+	nombre = input ("Introduce tu nombre:\n")
+	print("hola",nombre)
+else : 
+	print("Este programa es para mayores")
+print("Hasta pronto!!")
